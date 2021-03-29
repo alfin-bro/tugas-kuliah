2addc0af5295cf3e4f9e5e18c5a242e64535e10c
alfin-bro <alfinrafi2@gmail.com>
2021-03-29 15:18:12 +0700

    Create volume_bak.py

---
+++ volume_bak.py
@@ -0,0 +1,16 @@
+# MENGHITUNG VOLUME BAK
+
+# MENDEKLARASIKAN VARIABLE
+panjang = 5
+lebar = 6
+tinggi = 3
+
+# MENAMPILKAN PANJANG, LEBAR DAN TINGGI
+print(f"Panjang: {panjang}")
+print(f"Lebar: {lebar}")
+print(f"Tinggi: {tinggi}")
+
+# MENDEKLARASIKAN RUMUS VOLUME BAK
+# VOLUME = PxLxT
+volume = panjang*lebar*tinggi
+print(f"Volume bak adalah = {volume}")
